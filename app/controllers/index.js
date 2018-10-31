26a6599cf0160940167577d28db1af0eb586957d
ruochen.xie <ruochen.xie@bitmain.com>
2018-10-31 18:03:45 +0800

    模糊搜索功能（使用正则实现，复用了分类的列表页）

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -24,34 +24,55 @@ exports.search = function (req, res) {
   console.log('user in session: ', req.session.user)
 
   var catId = req.query.cat
-  var page = Number(req.query.p)
+  var page = Number(req.query.p) || 0
+  var q = req.query.q
 
   var count = 1
   var index = page * count
 
-  Category
-    .find({
-      _id: catId
-    })
-    .populate({
-      path: 'movies',
-      select: 'title poster',
-    })
-    .exec(function (err, categories) {
+  if (catId) {
+    Category
+      .find({
+        _id: catId
+      })
+      .populate({
+        path: 'movies',
+        select: 'title poster',
+      })
+      .exec(function (err, categories) {
+        if (err) {
+          console.log(err)
+        }
+        var category = categories[0] || []
+        var movies = category.movies || {}
+        var results = movies.slice(index, index + count)
+
+        res.render('results', {
+          title: 'imooc 结果列表页面',
+          keyword: category.name,
+          currentPage: page + 1,
+          query: 'cat=' + catId,
+          totalPage: Math.ceil(movies.length / count),
+          movies: results,
+        })
+      })
+  } else {
+    // 来自于搜索框提交的请求
+    Movie.find({title: new RegExp(q, 'i')}).exec(function(err, movies) {
       if (err) {
         console.log(err)
       }
-      var category = categories[0] || []
-      var movies = category.movies || {}
+
       var results = movies.slice(index, index + count)
 
       res.render('results', {
-        title: 'imooc 结果列表页面',
-        keyword: category.name,
+        title: 'imooc 搜索结果页面',
+        keyword: q,
         currentPage: page + 1,
-        query: 'cat=' + catId,
+        query: 'q=' + q,
         totalPage: Math.ceil(movies.length / count),
         movies: results,
       })
     })
+  }
 }
