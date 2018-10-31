46fdf63d5a24132b6aef9416620ef83cc82c7eef
ruochen.xie <ruochen.xie@bitmain.com>
2018-10-31 17:49:44 +0800

    增加分类的列表页以及其分页功能

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -19,3 +19,39 @@ exports.index = function (req, res) {
       })
     })
 }
+
+exports.search = function (req, res) {
+  console.log('user in session: ', req.session.user)
+
+  var catId = req.query.cat
+  var page = Number(req.query.p)
+
+  var count = 1
+  var index = page * count
+
+  Category
+    .find({
+      _id: catId
+    })
+    .populate({
+      path: 'movies',
+      select: 'title poster',
+    })
+    .exec(function (err, categories) {
+      if (err) {
+        console.log(err)
+      }
+      var category = categories[0] || []
+      var movies = category.movies || {}
+      var results = movies.slice(index, index + count)
+
+      res.render('results', {
+        title: 'imooc 结果列表页面',
+        keyword: category.name,
+        currentPage: page + 1,
+        query: 'cat=' + catId,
+        totalPage: Math.ceil(movies.length / count),
+        movies: results,
+      })
+    })
+}
--- config/routes.js
+++ config/routes.js
@@ -50,6 +50,7 @@ module.exports = function (app) {
   app.get('/admin/category/new', User.signinRequired, User.adminRequired, Category.new)
   app.post('/admin/category/new', User.signinRequired, User.adminRequired, Category.save)
   app.get('/admin/category/list', User.signinRequired, User.adminRequired, Category.list)
+  app.get('/results', Index.search)
 
 
   app.listen(3000, function () {
